011c49fd689df041b9ae3626a75dd16bfb79cf77
Thái TĐ <nhikute987654321@gmail.com>
2025-06-23 20:44:06 +0700

    create component

---
+++ src/BaiTapLayoutComponent/Body.jsx
@@ -0,0 +1,15 @@
+// Sidebar.js
+import React from 'react';
+import Banner from './Banner';
+import Items from './Items';
+
+const Body = () => {
+    return (
+        <>
+            <Banner />
+            <Items />
+        </>
+    );
+};
+
+export default Body;
